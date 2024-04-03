50be7eaa1d2863b00ed4ca498dd133a09ead8de6
Shaline Raghupathy <shalineraghupathy02@gmail.com>
2024-04-03 02:18:11 +0100

    add git ignore

--- MoveOn/src/entity/Player.java
+++ MoveOn/src/entity/Player.java
@@ -2,6 +2,7 @@ package entity;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
 import javax.imageio.ImageIO;
@@ -18,9 +19,14 @@ public class Player extends Entity{
 	public Player(GamePanel gp, KeyHandler keyH) {
 		this.gp = gp;
 		this.keyH = keyH;
-		
 		screenX = gp.screenWidth/2 - (gp.tileSize/2);
 		screenY = gp.screenHeight/2 - (gp.tileSize/2);
+		
+		solidArea = new Rectangle();
+		solidArea.x = 8;
+		solidArea.y = 16;
+		solidArea.width=32;
+		solidArea.height = 32;
 		setDefaultValues();
 		getPlayerImage();
 	}
