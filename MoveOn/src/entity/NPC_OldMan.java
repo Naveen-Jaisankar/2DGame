47e64a2a6243af1a7e0fef7039da76be62ae0152
Shaline Raghupathy <shalineraghupathy02@gmail.com>
2024-04-14 05:17:34 +0100

    Add game fonts and rendering

--- MoveOn/src/entity/NPC_OldMan.java
+++ MoveOn/src/entity/NPC_OldMan.java
@@ -29,7 +29,7 @@ public class NPC_OldMan extends Entity{
 	public void setDialogue(){
 		dialogues[0] = "Hello!, Lad.";
 		dialogues[1] = "So you've come to this island \nto find the treasure!";
-		dialogues[2] = "I used to be a great wizard but now...\nI'm bit too old for taking an adventure.";
+		dialogues[2] = "I used to be a great wizard \nbut now... I'm bit too old \nfor taking an adventure.";
 		dialogues[3] = "Well, good luck on you.";
 	}
 	public void setAction() {
--- MoveOn/src/main/UI.java
+++ MoveOn/src/main/UI.java
@@ -3,7 +3,11 @@ package main;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontFormatException;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.DecimalFormat;
 
 import object.OBJ_Key;
@@ -11,7 +15,7 @@ import object.OBJ_Key;
 public class UI {
 	GamePanel gp;
 	Graphics2D g2;
-	Font arial_40,arial_80B;
+	Font maruMonica, purisaB;
 	public boolean messageOn = Boolean.FALSE;
 	public String message = "";
 	int messageCounter = 0;
@@ -22,8 +26,20 @@ public class UI {
 	
 	public UI(GamePanel gp) {
 		this.gp = gp;
-		arial_40 =  new Font("Arial",Font.PLAIN,40);
-		arial_80B =  new Font("Arial",Font.BOLD,80);
+		
+		try{
+			InputStream is = getClass().getResourceAsStream("/font/MaruMonica.ttf");
+			maruMonica = Font.createFont(Font.TRUETYPE_FONT, is);
+			is = getClass().getResourceAsStream("/font/PurisaBold.ttf");
+			purisaB = Font.createFont(Font.TRUETYPE_FONT, is);
+		}
+		catch(FontFormatException e){
+			e.printStackTrace();
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+		
 
 	}
 	
@@ -33,78 +49,11 @@ public class UI {
 		messageOn = true;
 	}
 	
-	/**
-	 * This draw method is the v1.
-	public void draw(Graphics2D g2) {
-		
-		if(gameFinished) {
-			g2.setFont(arial_40);
-			g2.setColor(Color.white);
-			String text;
-			int textLength;
-			int gameCenterX;
-			int gameCenterY;
-
-			//TODO Implement the following line of code in a function
-			text = "You found the treasure";
-			textLength = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
-			
-			gameCenterX = gp.screenWidth/2 - textLength/2; // aligning the text to the center of the screen 
-			gameCenterY = gp.screenHeight/2 - (gp.tileSize*3);
-			
-			g2.drawString(text, gameCenterX, gameCenterY);
-			
-			
-			text = "Your Time is :" + dformat.format(playTime) + "!";
-			textLength = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
-			
-			gameCenterX = gp.screenWidth/2 - textLength/2; // aligning the text to the center of the screen 
-			gameCenterY = gp.screenHeight/2 + (gp.tileSize*4);
-			
-			g2.drawString(text, gameCenterX, gameCenterY);
-			
-			
-			g2.setFont(arial_80B);
-			g2.setColor(Color.white);
-			text = "Congratulations!!";
-			textLength = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
-			
-			gameCenterX = gp.screenWidth/2 - textLength/2; // aligning the text to the center of the screen 
-			gameCenterY = gp.screenHeight/2 + (gp.tileSize*3);
-			
-			g2.drawString(text, gameCenterX, gameCenterY);
-			
-			//TODO Move this to a sepearate function in gamePanel as well, and come up with some abstraction to make it more secure as it stops the game thread.
-			gp.gameThread = null;
-			
-			
-		}else {
-			g2.setFont(arial_40);
-			g2.setColor(Color.white);
-//			g2.drawImage(keyImage, gp.tileSize/2, gp.tileSize/2,gp.tileSize,gp.tileSize,null);
-			g2.drawString("x "+ gp.player.hasKey,74, 65);
-			
-			//Time
-			playTime += (double)1/60;
-			g2.drawString("Time:"+ dformat.format(playTime), gp.tileSize*11, 65);
-			
-			if(messageOn) {
-				g2.setFont(g2.getFont().deriveFont(30F));
-				g2.drawString(message, gp.tileSize/2, gp.tileSize*5);
-				messageCounter++;
-				
-				if(messageCounter > 120) {
-					messageCounter = 0;
-					messageOn = false;
-				}
-			}
-		}
-	}
-	**/
 	
 	public void draw(Graphics2D g2) {
 		this.g2 =g2;
-		g2.setFont(arial_40);
+		g2.setFont(purisaB);
+		g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 		g2.setColor(Color.white);
 		
 		// Playstate
@@ -128,7 +77,7 @@ public class UI {
 		int width = gp.screenWidth - (gp.tileSize*4);
 		int height = gp.tileSize*4;
 		drawSubWindow(x,y,width,height);
-		g2.setFont(g2.getFont().deriveFont(Font.PLAIN,28F));
+		g2.setFont(g2.getFont().deriveFont(Font.PLAIN,24F));
 		x+=gp.tileSize;
 		y+=gp.tileSize;
 		for(String line : currentDialogue.split("\n")){
