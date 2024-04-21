09ddc7c64dae86b61c8634844577d13d50081e98
Shaline Raghupathy <shalineraghupathy02@gmail.com>
2024-04-21 04:11:25 +0100

    Implement enhanced monster movements

--- MoveOn/res/monster/MON_GreenSlime.java
+++ MoveOn/res/monster/MON_GreenSlime.java
@@ -39,26 +39,129 @@ public class MON_GreenSlime extends Entity {
         right2 = setup("/monster/greenslime_down_2",gp.tileSize,gp.tileSize);
 
     }
-    public void setAction(){
-        actionLockCounter++;
-		if(actionLockCounter ==120) {
-			Random random = new Random();
-            int i = random.nextInt(100)+1;
-            if(i<=25) {
-                direction = "up";		
-                }
-            if(i>25 && i<=50) {
-                direction = "down";		
-                }
-            if(i>50 && i<=75) {
-                direction = "left";		
-                }
-            if(i>75 && i<=100) {
-                direction = "up";		
-                }
-            actionLockCounter =0;
-            }
+    // public void setAction(){
+    //     actionLockCounter++;
+	// 	if(actionLockCounter ==120) {
+	// 		Random random = new Random();
+    //         int i = random.nextInt(100)+1;
+    //         if(i<=25) {
+    //             direction = "up";		
+    //             }
+    //         if(i>25 && i<=50) {
+    //             direction = "down";		
+    //             }
+    //         if(i>50 && i<=75) {
+    //             direction = "left";		
+    //             }
+    //         if(i>75 && i<=100) {
+    //             direction = "up";		
+    //             }
+    //         actionLockCounter =0;
+    //         }
 
-    }
+    // }
+
+    public void setAction() {
+		int xDistance;
+		int yDistance;
+		int Distance;
+		
+		for(int i = 0; i < gp.monster.length; i++) {
+			if(gp.monster[i] != null) {
+				xDistance = Math.abs(gp.player.worldX - gp.monster[i].worldX);
+				yDistance = Math.abs(gp.player.worldY - gp.monster[i].worldY);
+				Distance = Math.max(xDistance, yDistance);
+				if(gp.monster[i].alive == true && gp.monster[i].dying ==false) {
+					if(Distance < 4*gp.tileSize) {
+						if(gp.player.worldX > gp.monster[i].worldX && gp.player.worldY > gp.monster[i].worldY) {
+							if(changeDirection1 < 30) {
+								gp.monster[i].direction = "right";
+							}
+							else if(changeDirection1 >=45 && changeDirection1 < 90) {
+								gp.monster[i].direction = "down";
+							}
+							else if(changeDirection1 > 90){
+								changeDirection1 = 0;
+							}
+							changeDirection1++;
+						}
+						else if(gp.player.worldX > gp.monster[i].worldX && gp.player.worldY < gp.monster[i].worldY) {
+							if(changeDirection2 < 45) {
+								gp.monster[i].direction = "right";
+							}
+							else if(changeDirection2 >=45 && changeDirection2 < 90) {
+								gp.monster[i].direction = "up";
+							}
+							else if(changeDirection2 > 90){
+								changeDirection2 = 0;
+							}
+							changeDirection2++;
+						}
+						else if(gp.player.worldX < gp.monster[i].worldX && gp.player.worldY > gp.monster[i].worldY) {
+							if(changeDirection3 < 45) {
+								gp.monster[i].direction = "left";
+							}
+							else if(changeDirection3 >=45 && changeDirection3 < 90) {
+								gp.monster[i].direction = "down";
+							}
+							else if(changeDirection3 > 90){
+								changeDirection3 = 0;
+							}
+							changeDirection3++;
+						}
+						else if(gp.player.worldX < gp.monster[i].worldX && gp.player.worldY < gp.monster[i].worldY) {
+							if(changeDirection4 < 45) {
+								gp.monster[i].direction = "left";
+							}
+							else if(changeDirection4 >=45 && changeDirection4 < 90) {
+								gp.monster[i].direction = "up";
+							}
+							else if(changeDirection4 > 90){
+								changeDirection4 = 0;
+							}
+							changeDirection4++;
+						}
+						else if(gp.player.worldX == gp.monster[i].worldX && gp.player.worldY < gp.monster[i].worldY) {
+							gp.monster[i].direction = "up";
+						}
+						else if(gp.player.worldX == gp.monster[i].worldX && gp.player.worldY > gp.monster[i].worldY) {
+							gp.monster[i].direction = "down";
+						}
+						else if(gp.player.worldX < gp.monster[i].worldX && gp.player.worldY == gp.monster[i].worldY) {
+							gp.monster[i].direction = "left";
+						}
+						else if(gp.player.worldX > gp.monster[i].worldX && gp.player.worldY == gp.monster[i].worldY) {
+							gp.monster[i].direction = "right";
+						}
+						
+					}
+					else {
+						actionLockCounter++;
+						
+						if(actionLockCounter == 600) {
+						
+						Random random = new Random();
+						int a = random.nextInt(100)+1; //picks up a number from 1 to 100
+						
+						if(a <= 25) {
+							direction = "up";
+						}
+						else if(a > 25 && a <= 50) {
+							direction = "left";
+						}
+						else if(a > 50 && a <= 75) {
+							direction = "down";
+						}
+						else if(a > 75 && a <= 100) {
+							direction = "right";
+						}
+						actionLockCounter = 0;
+						}
+					}
+				}				
+			}
+		}	
+	}
+
 
 }
